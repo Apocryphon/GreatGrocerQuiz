05b0d5e2b60a3e277f454de18aad67473b351067
Richard Yeh <apocryphon.now@gmail.com>
2017-04-08 20:19:35 -0700

    wip * Questions and Answers shuffled * changed answer image views to buttons, keep track of currently selected answer

--- Grocer Quiz/INCQuizViewController.m
+++ Grocer Quiz/INCQuizViewController.m
@@ -9,23 +9,24 @@
 #import "INCQuizViewController.h"
 #import "INCQuestion.h"
 
-#import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/UIButton+WebCache.h>
 
 @interface INCQuizViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 
-@property (weak, nonatomic) IBOutlet UIImageView *answerView1;
-@property (weak, nonatomic) IBOutlet UIImageView *answerView2;
-@property (weak, nonatomic) IBOutlet UIImageView *answerView3;
-@property (weak, nonatomic) IBOutlet UIImageView *answerView4;
+@property (weak, nonatomic) IBOutlet UIButton *answerButton1;
+@property (weak, nonatomic) IBOutlet UIButton *answerButton2;
+@property (weak, nonatomic) IBOutlet UIButton *answerButton3;
+@property (weak, nonatomic) IBOutlet UIButton *answerButton4;
 
 @property (weak, nonatomic) IBOutlet UILabel *promptLabel;
 @property (weak, nonatomic) IBOutlet UIButton *submitButton;
 
 @property (assign, nonatomic) BOOL isQuizInProgress;
 @property (assign, nonatomic) int currentQuestionNumber;
+@property (assign, nonatomic) NSInteger selectedAnswerNumber;
 
-@property (strong, nonatomic) NSMutableArray *questionsArray;
+@property (copy, nonatomic) NSArray *questionsArray;
 
 @end
 
@@ -37,6 +38,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
+  self.selectedAnswerNumber = 0;
+  
   [self retrieveQuestions];
 }
 
@@ -47,13 +50,49 @@
   NSDictionary *jsonQuestionsDictionary = [NSJSONSerialization JSONObjectWithData:jsonData
                                                                           options:0
                                                                             error:nil];
-  self.questionsArray = [NSMutableArray arrayWithCapacity:[jsonQuestionsDictionary count]];
+  NSMutableArray *tempQuestionsArray = [NSMutableArray arrayWithCapacity:[jsonQuestionsDictionary count]];
   
   for (NSString *itemName in jsonQuestionsDictionary) {
     INCQuestion *newQuestion = [[INCQuestion alloc] initWithName:itemName
                                                         urlArray:jsonQuestionsDictionary[itemName]];
-    [self.questionsArray addObject:newQuestion];
+    NSMutableArray *copyOfImageUrlArray = [newQuestion.imageUrlArray mutableCopy];
+    newQuestion.imageUrlArray = [[self shuffleArray:copyOfImageUrlArray] copy];
+    [tempQuestionsArray addObject:newQuestion];
   }
+  
+  self.questionsArray = [[self shuffleArray:tempQuestionsArray] copy];
+
+}
+
+// Fisher-Yates implementation
+- (NSMutableArray *)shuffleArray:(NSMutableArray *)array {
+  for (NSUInteger i = [array count] - 1; i > 0 ; i--) {
+    NSUInteger firstIndex = i;
+    NSUInteger secondIndex = arc4random() % (i+1);
+    [array exchangeObjectAtIndex:firstIndex withObjectAtIndex:secondIndex];
+  }
+  return array;
+}
+
+
+- (IBAction)selectedAnswer:(id)sender {
+  UIButton *nowSelectedButton = (UIButton *)sender;
+
+  // do nothing if same as currently selected answer
+  if (self.selectedAnswerNumber == nowSelectedButton.tag) {
+    return;
+  }
+
+  if (self.selectedAnswerNumber > 0) {
+    UIButton *previouslySelectedButton = (UIButton *)[self.view viewWithTag:self.selectedAnswerNumber];
+    previouslySelectedButton.selected = NO;
+    // TODO: change appearance of deselected former answer
+  }
+  
+  self.selectedAnswerNumber = nowSelectedButton.tag;
+  nowSelectedButton.selected = YES;
+  
+  // TODO: change appearance of selected answer
 }
 
 - (void)didReceiveMemoryWarning {
@@ -74,33 +113,53 @@
 - (void)updateForQuestion {
   INCQuestion *currentQuestion = self.questionsArray[self.currentQuestionNumber-1];
   self.promptLabel.text = [NSString stringWithFormat:@"Select the %@", currentQuestion.itemName];
-  [self.answerView1 sd_setImageWithURL:[NSURL URLWithString:currentQuestion.imageUrlArray[0]]
-                      placeholderImage:[UIImage imageNamed:@"groceries-placeholder.png"]];
-  [self.answerView2 sd_setImageWithURL:[NSURL URLWithString:currentQuestion.imageUrlArray[1]]
-                      placeholderImage:[UIImage imageNamed:@"groceries-placeholder.png"]];
-  [self.answerView3 sd_setImageWithURL:[NSURL URLWithString:currentQuestion.imageUrlArray[2]]
-                      placeholderImage:[UIImage imageNamed:@"groceries-placeholder.png"]];
-  [self.answerView4 sd_setImageWithURL:[NSURL URLWithString:currentQuestion.imageUrlArray[3]]
-                      placeholderImage:[UIImage imageNamed:@"groceries-placeholder.png"]];
+  
+  NSArray *answerButtonsArray = @[self.answerButton1, self.answerButton2, self.answerButton3, self.answerButton4];
 
-
+  int answerCounter = 0;
+  for (UIButton *button in answerButtonsArray) {
+    [button sd_setImageWithURL:[NSURL URLWithString:currentQuestion.imageUrlArray[answerCounter]]
+                      forState:UIControlStateNormal
+              placeholderImage:[UIImage imageNamed:@"groceries-placeholder.png"]];
+    answerCounter++;
+  }
   
 }
-
 
 - (IBAction)startQuiz:(id)sender {
   self.isQuizInProgress = YES;
   self.startButton.hidden = YES;
-  self.answerView1.hidden = NO;
-  self.answerView2.hidden = NO;
-  self.answerView3.hidden = NO;
-  self.answerView4.hidden = NO;
+  self.answerButton1.hidden = NO;
+  self.answerButton2.hidden = NO;
+  self.answerButton3.hidden = NO;
+  self.answerButton4.hidden = NO;
   self.promptLabel.hidden = NO;
   self.submitButton.hidden = NO;
   self.currentQuestionNumber = 1;
   
   [self updateForQuestion];
 }
+
+- (IBAction)submitAnswer:(id)sender {
+  UIAlertController *confirmAnswerAlert = [UIAlertController alertControllerWithTitle:@"Submit Answer" message:@"Are you sure of your answer?" preferredStyle:UIAlertControllerStyleAlert];
+  [confirmAnswerAlert addAction:[UIAlertAction actionWithTitle:@"No"
+                                                         style:UIAlertActionStyleCancel
+                                                       handler:nil]];
+  __weak typeof (self) weakSelf = self;
+  [confirmAnswerAlert addAction:[UIAlertAction actionWithTitle:@"Yes"
+                                                         style:UIAlertActionStyleDefault
+                                                       handler:^(UIAlertAction *action){
+                                                         [weakSelf saveAnswer];
+                                                       }]
+   ];
+
+  [self presentViewController:confirmAnswerAlert animated:YES completion:nil];
+}
+
+- (void)saveAnswer {
+  
+}
+
 
 
 @end
